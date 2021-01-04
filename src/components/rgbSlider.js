20e49f6271f7f2482af512fe7a6aa121f595f78e
Matthew Ricklefs <72839408+matthewricklefs@users.noreply.github.com>
2021-01-04 11:31:13 -0500

    finalizing the RGB slider component

--- src/components/rgbSlider.js
+++ src/components/rgbSlider.js
@@ -4,13 +4,58 @@ export default class Slider extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
+    const { red, green, blue } = this.state;
     return (
       <div>
-        <h1 onClick={this.state}>Hello from Slider</h1>
+        <div
+          style={{
+            height: 50,
+            width: 100,
+            backgroundColor: `rgb(${red}, ${green}, ${blue})`
+          }}
+        />
+        <p> {`rgb(${red}, ${green}, ${blue})`} </p>
+
+        <input
+          type="range"
+          name="red"
+          min="0"
+          max="255"
+          value={red}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="range"
+          name="green"
+          min="0"
+          max="255"
+          value={green}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="range"
+          name="blue"
+          min="0"
+          max="255"
+          value={blue}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
