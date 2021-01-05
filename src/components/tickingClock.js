e4f517d2c728b423f3ec474fcf1aa4e963f8c1b7
Matthew Ricklefs <72839408+matthewricklefs@users.noreply.github.com>
2021-01-05 17:26:13 -0500

    changing the Clock from a Class based Component into a functional component

--- src/components/tickingClock.js
+++ src/components/tickingClock.js
@@ -1,34 +1,21 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-export default class Clock extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date()
-    };
-  }
-  // reset the date by updating the state of it
-  tick() {
-    this.setState({
-      time: new Date()
-    });
-  }
-  // when the component mounts you set a timerid and set the interval
-  // to the tick method over 1000 milliseconds
-  componentDidMount() {
-    this.clockhand = setInterval(() => {
-      this.tick();
-    }, 1000);
-  }
-  // clear the the clock every time it is about to unmount and then rerender
-  componentWillUnmount() {
-    clearInterval(this.clockhand);
-  }
-  render() {
-    return (
-      <div>
-        <h1> {this.state.time.toLocaleTimeString()}</h1>
-      </div>
-    );
-  }
+export default function Clock(props) {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    let interval = setInterval(() => tick(), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const tick = () => {
+    setTime(new Date());
+  };
+
+  return (
+    <div>
+      <h1>{time.toLocaleTimeString()}</h1>
+    </div>
+  );
 }
